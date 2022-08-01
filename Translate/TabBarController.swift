58cb109e3d9953fa737fd2acdf1c3dd5f20572da
Joe <nastillmatic@gmail.com>
2022-08-01 17:27:52 +0900

    번역탭 UI 작업

---
+++ Translate/TabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Translate
+//
+//  Created by HwangByungJo  on 2022/08/01.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let translateViewController = TransLateViewController()
+    translateViewController.tabBarItem = UITabBarItem(title: "번역",
+                                                      image: UIImage(systemName: "mic"),
+                                                      selectedImage: UIImage(systemName: "mic.fill"))
+    
+    let bookmarkViewController = UIViewController()
+    bookmarkViewController.tabBarItem = UITabBarItem(title: "즐겨찾기",
+                                                    image: UIImage(systemName: "star"),
+                                                    selectedImage: UIImage(systemName: "star.fill"))
+    
+    viewControllers = [translateViewController, bookmarkViewController]
+    
+    
+  }
+  
+  
+}
+
---
+++ Translate/UIColor+.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+.swift
+//  Translate
+//
+//  Created by HwangByungJo  on 2022/08/01.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  static var mainTintColor = UIColor.systemPink
+  
+}
